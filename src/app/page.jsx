b0ad970cf49d482833f96d18271bcaafa6c8c360
Kwangsu-CHOI <wisecks@gmail.com>
2023-07-20 15:51:25 +1000

    after deploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,20 +4,18 @@ import Hero from "public/hero.png";
 import Button from "@/components/button/Button";
 
 export default function Home() {
-	// return (
-	// 	<div className={styles.container}>
-	// 		<div className={styles.item}>
-	// 			<h1 className={styles.title}>Better design for your better Life.</h1>
-	// 			<p className={styles.desc}>
-	// 				Turning your briliant idea into Reality. It changes your life!
-	// 			</p>
-	// 			<Button url="/portfolio" text="See more now" />
-	// 		</div>
-	// 		<div className={styles.item}>
-	// 			<Image src={Hero} alt="" className={styles.img} />
-	// 		</div>
-	// 	</div>
-	// );
-
-	return <div>TEMP</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.item}>
+				<h1 className={styles.title}>Better design for your better Life.</h1>
+				<p className={styles.desc}>
+					Turning your briliant idea into Reality. It changes your life!
+				</p>
+				<Button url="/portfolio" text="See more now" />
+			</div>
+			<div className={styles.item}>
+				<Image src={Hero} alt="" className={styles.img} />
+			</div>
+		</div>
+	);
 }
